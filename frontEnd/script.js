028674510160e6d2f76fc3e995ce90f5a4c82761
Victor Emanuel Ferreira <victoremanueldsf@gmail.com>
2024-03-18 20:24:41 -0300

    Foi adicinado o modelo de IA no sistema. Foi tambem dado a continuidade do js para os botoes de mudar para enviar o link, enviar o texto da noticia.

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,46 +1,22 @@
 
 let pelo_link = document.getElementById("botao_pelo_link")
 let pelo_texto = document.getElementById("botao_pelo_texto")
-
-console.log(pelo_link)
-
-function acionar_esconder_css(botao){
-
-    let controla_css_textarea = document.getElementById("conteudo_da_noticia")
-    let controla_css_input_titulo = document.getElementById("input_titulo")
-    let controla_css_input_link = document.getElementById("input_link")
-
-    let display_textarea = "none"
-    let display_titulo = "none"
-
-    if(botao == pelo_texto){
-
-        let  display_textarea = "hidden"
-        let  display_titulo = "hidden"
-    } 
-
-
-    botao.addEventListener("click", function(){
-        
-        controla_css_elemento.style.display = display_textarea
-
-    
-    
-    } )
-
-
-} 
-
-
-
+let controla_css_elemento = document.getElementById("conteudo_da_noticia")
+let controla_css_input_titulo = document.getElementById("input_titulo")
+let controla_css_input_link = document.getElementById("input_link")
 
 pelo_link.addEventListener("click", function(){
-
-    let controla_css_elemento = document.getElementById("conteudo_da_noticia")
-
-    console.log(controla_css_elemento)
-
+    
     controla_css_elemento.style.display = "none"
+    controla_css_input_titulo.style.display = "none"
+    controla_css_input_link.style.display = "block"
+} )
 
+
+pelo_texto.addEventListener("click", function(){
+
+    controla_css_elemento.style.display = "block"
+    controla_css_input_titulo.style.display = "block"
+    controla_css_input_link.style.display = "none"
 
 } )
